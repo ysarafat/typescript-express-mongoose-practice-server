d4284f687b9267509072ca92a617c01da45201eb
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-11-17 19:44:22 +0600

    create student type, schema and model

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,36 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type StudentName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+export type Student = {
+  id: string;
+  name: StudentName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  avatar?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'O+' | 'O-' | 'AB+' | 'AB-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  isActive: 'active' | 'inactive';
+};
---
+++ src/modules/student/student.model.ts
@@ -0,0 +1,100 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  StudentName,
+} from './student.interface';
+
+const studentNameSchema = new Schema<StudentName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+const localGuardian = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+  },
+  occupation: {
+    type: String,
+  },
+  contactNo: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+});
+const StudentSchema = new Schema<Student>({
+  id: {
+    type: String,
+  },
+  name: studentNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  emergencyContactNo: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'],
+  permanentAddress: {
+    type: String,
+    required: true,
+  },
+  presentAddress: {
+    type: String,
+    required: true,
+  },
+  guardian: guardianSchema,
+  localGuardian: localGuardian,
+  isActive: ['active', 'inactive'],
+});
+
+const Student = model<Student>('Student', StudentSchema);
+
+export default Student;
